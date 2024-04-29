9f78483ff4b2e097c5353eda2bce1137b4e13868
Raman Datta <ramandatta@gmail.com>
2024-04-29 11:58:19 +0800

    added routes, static folder and basic html page for login sign up

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", login=False)
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
