f6eaf6c23e0e43592f55ce30ad990b9f2df2c995
Raman Datta <ramandatta87@gmail.com>
2024-04-30 11:35:44 +0800

    Added Config.py into __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask 
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)      # Applying configuration from Config.py
 
-from app import routes
+from app import routes              # Added  routes from routes.py
