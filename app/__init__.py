7caecd568cdad5cbc8e5ac74cb64873c0b87aefa
Raman Datta <ramandatta87@gmail.com>
2024-05-06 21:33:22 +0800

    Modified __init.py inside app folder to include controller and blueprints.

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float
-from .config import Config 
-from .models import db, User  # Import db here from models
 from flask_migrate import Migrate
+from config import Config
 
-
-# Initialize the SQLAlchemy instance without passing the app
-#db = SQLAlchemy()
+db = SQLAlchemy()
 migrate = Migrate()
+
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_class)  # Load configuration
+    app.config.from_object(config_class)
 
-    db.init_app(app)  # Link the SQLAlchemy instance to the Flask app
-
+    db.init_app(app)
     migrate.init_app(app, db)
 
- 
-    # Importing models here ensures they're aware of the db instance
-    from .models import User
-
-    # Register routes
-    from .routes import init_routes
-    init_routes(app)  # Assuming routes are set up to accept an app instance
-
-    # Optional: Create database tables within an app context
-    with app.app_context():
-        db.create_all()
+    from app.controllers.auth_controller import auth
+    from app.controllers.main_controller import main
+    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(main)
 
     return app
