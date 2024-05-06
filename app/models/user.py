50230547e856c751478f55401a134187cce5f2da
Raman Datta <ramandatta87@gmail.com>
2024-05-06 22:24:23 +0800

    Added Appropriate comments in user.py model.

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,14 @@
+# Import necessary modules and functions(for password hashing and salting)
 from app import db
 from werkzeug.security import generate_password_hash
 
+#Defining the User Model Class
+
 class User(db.Model):
+    #Table Name
     __tablename__ = "users"
+
+    #Defining the columns
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
@@ -11,8 +17,10 @@ class User(db.Model):
     major = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
 
+    #Method to securely set the user's password
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    # Method to provide a string representation of the User object
     def __repr__(self):
         return f'<User {self.email}>'
