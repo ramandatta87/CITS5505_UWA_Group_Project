f28e0965de86f30c80f5e8aea7c22991624bb698
Raman Datta <ramandatta87@gmail.com>
2024-05-07 22:05:44 +0800

    added email validator

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class RegisterForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
